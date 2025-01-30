320a389c045f3f506800680785f35fdb86cebfb0
Madhavan C <139116570+madhavan521@users.noreply.github.com>
2025-01-30 10:22:35 +0530

    Update ProtectRouter.js

--- Middleware/ProtectRouter.js
+++ Middleware/ProtectRouter.js
@@ -19,6 +19,8 @@ const ProtectRouter = async (req, res, next) => {
 		if (!user) {
 			return res.status(404).json({ error: "User not found" });
 		}
+			console.log(user)
+
 
 		req.user = user;
 		next();
